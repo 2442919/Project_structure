20ea43940cc90fff5f35c7bf2a0a1f95bc7230ee
N151P34NU D3C384L V1093L <33691956+2442919@users.noreply.github.com>
2024-11-22 09:38:07 +0100

    Create Gimp_structure.sh

---
+++ Gimp_structure.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+echo "Introduceți numele proiectului pentru GIMP:"
+read GIMP_PROJECT_NAME
+
+# Creare directoare GIMP
+mkdir -p "$GIMP_PROJECT_NAME/sources/raw/jpg"
+mkdir -p "$GIMP_PROJECT_NAME/sources/raw/png"
+mkdir -p "$GIMP_PROJECT_NAME/sources/raw/tiff"
+mkdir -p "$GIMP_PROJECT_NAME/sources/raw/psd"
+mkdir -p "$GIMP_PROJECT_NAME/sources/raw/xcf"
+
+mkdir -p "$GIMP_PROJECT_NAME/sources/edited/xcf"
+mkdir -p "$GIMP_PROJECT_NAME/sources/edited/psd"
+
+mkdir -p "$GIMP_PROJECT_NAME/exports/png/png"
+mkdir -p "$GIMP_PROJECT_NAME/exports/jpg/jpg"
+mkdir -p "$GIMP_PROJECT_NAME/exports/web/png"
+mkdir -p "$GIMP_PROJECT_NAME/exports/web/jpg"
+mkdir -p "$GIMP_PROJECT_NAME/exports/web/webp"
+
+mkdir -p "$GIMP_PROJECT_NAME/brushes/gbr"
+mkdir -p "$GIMP_PROJECT_NAME/brushes/vbr"
+mkdir -p "$GIMP_PROJECT_NAME/brushes/abr"
+
+mkdir -p "$GIMP_PROJECT_NAME/patterns/pat"
+mkdir -p "$GIMP_PROJECT_NAME/patterns/png"
+
+mkdir -p "$GIMP_PROJECT_NAME/fonts/ttf"
+mkdir -p "$GIMP_PROJECT_NAME/fonts/otf"
+
+mkdir -p "$GIMP_PROJECT_NAME/docs/md"
+mkdir -p "$GIMP_PROJECT_NAME/docs/txt"
+mkdir -p "$GIMP_PROJECT_NAME/docs/pdf"
+
+echo " Fisierele au fost create cu succes "
